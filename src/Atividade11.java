8c834573bd967a5d8a595b4bcacab1dbce9a9f4e
jabes <138511008+jabesz@users.noreply.github.com>
2023-11-09 19:07:42 -0300

    Update Atividade11.java

--- src/Atividade11.java
+++ src/Atividade11.java
@@ -5,32 +5,40 @@ public class Atividade11 {
         var input = new Scanner(System.in);
 
         int[] num =new int[10];
+
+        System.out.println("Informe 10 numeros inteiros");
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Informe " + (i + 1) + " numero: ");
+            num[i] = input.nextInt();
+        }
+
         int[] pares =new int[10];
         int[] impares =new int[10];
+
         int contPares = 0, contImpares = 0;
 
-        System.out.println("Informe 10 numeros inteiros:");
-
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Informe " +(i + 1)+ " numero: ");
-            num[i] = input.nextInt();
-
+        for (int i = 0; i < 10; i++){
             if (num[i] % 2 == 0){
-                pares[contPares] = num[i];
-                contPares++;
+                pares[contPares++] = num[i];
             } else {
-                impares[contImpares] = num[i];
-                contImpares++;
+                impares[contImpares++] = num[i];
             }
         }
 
         System.out.println("Numeros pares: ");
-        for (int i = 0; i < contPares; i++) {
-            System.out.println(pares[i] + " ");
-        }
+        exibirVetor(pares, contPares);
 
         System.out.println("\n numeros impares");
+        exibirVetor(impares, contImpares);
+
 
         input.close();
+    }
+
+    private static void exibirVetor(int[] vetor, int tamanho) {
+        for (int i = 0; i < tamanho; i++) {
+            System.out.print(vetor[i] + " \n");
+        }
+        System.out.println();
     }
 }
